0e6957d4730d7a0fe29d6be5aed8160cecbef1e0
Restyled.io <commits@restyled.io>
2020-02-03 23:45:47 +0000

    Restyled by clang-format

--- static/main.js
+++ static/main.js
@@ -1,9 +1,3 @@
-function openGitpod() {
-    window.open("https://www.gitpod.io/docs/");
-}
-function openGosh() {
-    window.open("https://github.com/gosh-terminal/gosh");
-}
-function openCliKit() {
-    window.open("https://github.com/JesterOrNot/cli_kit");
-}
+function openGitpod() { window.open("https://www.gitpod.io/docs/"); }
+function openGosh() { window.open("https://github.com/gosh-terminal/gosh"); }
+function openCliKit() { window.open("https://github.com/JesterOrNot/cli_kit"); }
