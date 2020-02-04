c4bbad02159c5cad437f66d31f7c808d52fc5950
Restyled.io <commits@restyled.io>
2020-02-04 02:06:50 +0000

    Restyled by clang-format

--- site/main.js
+++ site/main.js
@@ -1,18 +1,6 @@
-function openGitpod() {
-    window.open("https://www.gitpod.io/docs/");
-}
-function openGosh() {
-    window.open("https://github.com/gosh-terminal/gosh");
-}
-function openCliKit() {
-    window.open("https://github.com/JesterOrNot/cli_kit");
-}
-function goToWork() {
-    window.location.replace("work.html");
-}
-function goToHome() {
-    window.location.replace("index.html");
-}
-function goToSkills() {
-    window.location.replace("skills.html")
-}
+function openGitpod() { window.open("https://www.gitpod.io/docs/"); }
+function openGosh() { window.open("https://github.com/gosh-terminal/gosh"); }
+function openCliKit() { window.open("https://github.com/JesterOrNot/cli_kit"); }
+function goToWork() { window.location.replace("work.html"); }
+function goToHome() { window.location.replace("index.html"); }
+function goToSkills() { window.location.replace("skills.html") }
