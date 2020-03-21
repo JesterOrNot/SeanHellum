033ebfcb728d85da1e0e9e0866a98d674e84ca40
Restyled.io <commits@restyled.io>
2020-03-21 22:25:07 +0000

    Restyled by prettier

--- www/main.js
+++ www/main.js
@@ -1,8 +1,18 @@
-function openGitpod() { window.open("https://www.gitpod.io"); }
-function openGosh() { window.open("https://github.com/gosh-terminal/gosh"); }
+function openGitpod() {
+  window.open("https://www.gitpod.io");
+}
+function openGosh() {
+  window.open("https://github.com/gosh-terminal/gosh");
+}
 function openDockerfreeze() {
   window.open("https://github.com/gitpod-io/dockerfreeze");
 }
-function goToWork() { window.location.replace("/work"); }
-function goToHome() { window.location.replace("/"); }
-function goToSkills() { window.location.replace("/skills"); }
+function goToWork() {
+  window.location.replace("/work");
+}
+function goToHome() {
+  window.location.replace("/");
+}
+function goToSkills() {
+  window.location.replace("/skills");
+}
