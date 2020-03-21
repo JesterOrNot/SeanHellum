5b57cf9d8e893c2abb17b7e48cbe2c60d18dfbd0
Sean Hellum <seanhellum45@gmail.com>
2020-03-21 22:22:52 +0000

    move stuff around to make things more modular

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,37 @@ use std::path::{Path, PathBuf};
 
 #[get("/")]
 fn index() -> Result<NamedFile> {
-    NamedFile::open("site/index.html")
+    NamedFile::open("www/index.html")
 }
 
 #[get("/skills")]
 fn skills() -> Result<NamedFile> {
-    NamedFile::open("site/skills.html")
+    NamedFile::open("www/skills.html")
 }
 
 #[get("/work")]
 fn work() -> Result<NamedFile> {
-    NamedFile::open("site/work.html")
+    NamedFile::open("www/work.html")
 }
 
 #[catch(404)]
 fn error_not_found() -> Result<NamedFile> {
-    NamedFile::open("site/404.html")
+    NamedFile::open("www/404.html")
 }
 
-#[get("/site/<file..>")]
+#[get("/asset/<asset..>")]
+fn assets(asset: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("www/assets/").join(asset)).ok()
+}
+
+#[get("/www/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("site/").join(file)).ok()
+    NamedFile::open(Path::new("www/").join(file)).ok()
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, skills, work, files])
+        .mount("/", routes![index, skills, work, files, assets])
         .register(catchers![error_not_found])
         .launch();
 }
